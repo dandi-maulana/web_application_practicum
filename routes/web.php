55fbda0a3a5076b4d366c99e2abb4961e97efb14
dandi-maulana <nugraha.rahmadan1304@gmail.com>
2025-05-19 22:28:02 +0700

    Route and Controller Practicum

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\HelloController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HelloController::class, 'index']);
+Route::get('/hello/{name}', [HelloController::class, 'sayHello']);
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang Di Aplikasi Laravel';
+});
+
+Route::get('/hello/{name}', function ($name) {
+    return 'Hello, ' . $name . '!';
 });

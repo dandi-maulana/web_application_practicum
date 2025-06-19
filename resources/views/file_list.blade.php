7f39a0e0d570c137fe76a8e7b9128a22c917dd4d
Tyas Adrian <tyasadrian33@gmail.com>
2025-06-19 21:55:42 +0700

    sudah selesai

--- resources/views/file_list.blade.php
+++ resources/views/file_list.blade.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>File List</title>
+    <title>Daftar Gambar</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .img-wrapper {
+            flex: 1;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
 <div class="container mt-5">
-    <h2>Daftar File yang Telah Diupload</h2>
+    <h2 class="mb-4 text-center">Daftar File yang Telah Diupload</h2>
 
     @if(session('success'))
         <div class="alert alert-success">{{ session('success') }}</div>
@@ -15,7 +21,9 @@
     <ul class="list-group">
         @foreach ($files as $file)
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                <img href="{{ Storage::url($file) }}" target="_blank">{{ basename($file) }}</img>
+                <div class="img-wrapper">
+                    <img src="{{ Storage::url($file) }}" alt="Gambar" style="width: 150px; height: auto; object-fit: cover;">
+                </div>
                 <form action="{{ route('files.delete', basename($file)) }}" method="POST">
                     @csrf
                     @method('DELETE')
